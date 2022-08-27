0c297912bd0549237aa36af324739087739c8d4f
Alexandr <lininan@mail.ru>
2022-08-27 10:15:51 +0300

    Выполнил первое задание через целые числа

---
+++ Task1(10)/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 10 нужно решить через ЦЕЛЫЕ ЧИСЛА, без строчек
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string number1 = Convert.ToString(number);
+
+if (number1.Length != 3)
+{
+Console.Write("Вы указали не трехзначное число: ");
+Console.WriteLine(number);
+}
+
+else
+
+{number = number%100/10;
+Console.WriteLine("Вторая чифра введенного числа:" + " " + number);
+}
+
+
